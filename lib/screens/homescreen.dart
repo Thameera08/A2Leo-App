09d438bda6fb418efe94f6e88a50b54d9d258eb1
Thameera Dananjaya <84010024+Thameera08@users.noreply.github.com>
2024-11-02 21:29:26 +0530

    new update about home screen

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:leomd/components/mainCards.dart';
